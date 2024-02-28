676b1635b66fecd03c3cb227fee3e5bc92e6958d
Your Name <you@example.com>
2024-02-28 06:40:10 -0800

    Work In Progress

--- src/Lecture14.java
+++ src/Lecture14.java
@@ -7,12 +7,22 @@ public class Lecture14 {
     // Note: you will need to put some of your answers inside a comments
 
     // Put your answer for #1 here:
+    // Object-oriented programming creates objects that contain data and methods and you can use the objects
+    // like building blocks to form structures and programs.
+
+    // Procedural programming is when a set of tasks are completed in a linear or sequential fashion
 
     // Put your answer for #2 here:
+    // An object is an instance of a class meaning it is a specific version of a class that is can be utilized
+    // A class is a blueprint that we use to build our objects and give them functionality
 
     // Put your answer for #3 here:
+    // The state of a String object refers to the data it holds at a given time.
+    // The behavior of a String object refers to the methods and functionality it provides.
 
     // Put your answer for #5 here:
+    // The state for the calculator would be the numbers that it is doing a calculation on.
+    // The behavior for the calculator would be all of the calculations it can perform such as addition
 
     // Put your answer for #8 here:
 
